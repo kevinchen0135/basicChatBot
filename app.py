763c315d53ebf352de90f87a5dab63a95ebb43a7
kevinchen0135 <43007580+kevinchen0135@users.noreply.github.com>
2024-07-08 22:57:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
 
-tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
 
 
 app = Flask(__name__)
